d8665484de29d46ace0f829c1d29a584f10620c7
lemkas <lemov1980@yandex.ru>
2021-09-20 01:26:57 +0300

    added popup and form

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const modal = document.querySelector(".modal");
+const spanClose = document.querySelector(".close");
+
 const chair = {
   imgUrl:
     "https://wallytally.ru/upload/iblock/de1/de1fa37da355f7697215a9ee38accaa6.jpg",
@@ -5,6 +8,11 @@ const chair = {
   priceCard: 9900,
   btnText: "Купить",
 };
+
+chair.priceCard = new Intl.NumberFormat("ru-RU", {
+  style: "currency",
+  currency: "RUB",
+}).format(chair.priceCard);
 
 console.log(chair.priceCard);
 
@@ -31,6 +39,24 @@ const createEl = (elem, elClass, atName, atValue, content) => {
 };
 
 createCard();
+
+const btns = document.querySelectorAll(".card-btn");
+
+const openModal = () => {
+  modal.style.display = "block";
+  console.log("123");
+};
+
+const closeModal = (e) => {
+  modal.style.display = "none";
+};
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", openModal);
+});
+
+spanClose.addEventListener("click", closeModal);
+// window.addEventListener("click", closeModal);
 
 //const makeCard = () => {
 //   const card = document.createElement("div")
